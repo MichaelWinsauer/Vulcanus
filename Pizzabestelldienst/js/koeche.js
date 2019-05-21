164f8309dfc7b3ca684827413f576c0bb7c25f73
Benjamin Brandtner <37274870+BenjaminBrandtner@users.noreply.github.com>
2019-05-21 12:59:06 +0200

    In staff tables: Added column, linked JS

---
+++ Pizzabestelldienst/js/koeche.js
@@ -0,0 +1,1 @@
+//TODO: Analog zu lieferanten.js
---
+++ Pizzabestelldienst/js/lieferanten.js
@@ -0,0 +1,11 @@
+//TODO: Mit Ajax regelmäßig die Datenbank abfragen nach Bestellposition deren Status "Fertig gebacken" oder "geladen" und Lieferant "null" oder der eingeloggte Lieferant ist.
+//Tabelle mit diesen Daten erzeugen.
+//Je nach Status den richtigen Knopf verfügbar machen.
+//Auf Knopfdruck mit Ajax die Datenbank updaten
+//Fertig ausgelieferte Pizzen aus der Tabelle entfernen.
+
+$(document).ready(() =>
+{
+    $("#staffMemberName").text("Josefina");
+
+});
