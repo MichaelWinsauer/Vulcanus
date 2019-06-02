772688d6fe960dcb89c278a9af32d6051fa2edd4
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-06-02 17:04:20 +0200

    Pizza Template added + navbar responsive fix
    Former-commit-id: 51f1e8e3830aa0dc3ba9217329e076ba3ed88533

--- Pizzabestelldienst/js/script.js
+++ Pizzabestelldienst/js/script.js
@@ -3,9 +3,9 @@ $(window).ready(function() {
         var wScroll = $(window).scrollTop();
         var wHeight = $(window).height();
         if(wScroll + 100 > wHeight)
-            {
-                $('#navigation').addClass('scrolled');
-            }
+        {
+            $('#navigation').addClass('scrolled');
+        }
         else
             $('#navigation').removeClass('scrolled');
     })
