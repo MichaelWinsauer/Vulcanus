bc7d066624b36e2f76945b1c1ea5789de2a3ed10
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-06-03 21:38:26 +0200

    AJAX Database Connection
    Former-commit-id: e4e58eddea5110482338b05cbac982b4a28cb2e5

--- Pizzabestelldienst/js/script.js
+++ Pizzabestelldienst/js/script.js
@@ -1,4 +1,4 @@
-$(window).ready(function() {
+$(document).ready(function() {
     $(window).scroll(function() {
         var wScroll = $(window).scrollTop();
         var wHeight = $(window).height();
@@ -9,4 +9,26 @@ $(window).ready(function() {
         else
             $('#navigation').removeClass('scrolled');
     })
-})
+
+    $.ajax({
+        url: "../php/api/menu.php",
+        type: "POST",
+        dataType: "JSON",
+        success: function(res) {
+            
+            for(var i = 0; i < res.length;  i++)
+            {
+                var name = res[i]["Pizza"];
+                var zutaten = res[i]["Rezept"];
+                var preisK = res[i]["28cm"];
+                var preisG = res[i]["32cm"];
+
+                document.getElementById('PizzaContainer').innerHTML += createCard(name, zutaten, preisK, preisG);
+            }
+        }
+    });
+});
+
+function createCard(name, zutaten, preisK, preisG) {  
+    return "<div class='card my-5 mx-2' style='width: 18rem;'><img src='../images/" + name +".jpg' class='card-img-top'><div class='card-body'><h4 class='card-title pb-2'>" + name +"</h4><h6 class='card-text'>Zutaten:</h6></div><ul class='list-group list-group-flush'><li class='list-group-item'>" + zutaten + "</li></ul><div class='card-body'><form><div class='d-flex flex-column flex-wrap'><div class='d-flex justify-content-around'><div class='custom-control custom-radio'><input type='radio' id='Radio" + name +  "28cm' name='Radio" + name +"28cm' class='custom-control-input'><label class='custom-control-label' for='Radio" + name + "28cm'>28 cm</label></div><div class='custom-control custom-radio'><input type='radio' id='Radio" + name +"32cm' name='Radio" + name +"32cm' class='custom-control-input'></input><label class='custom-control-label' for='Radio" + name +"32cm'>32 cm</label></div></div><button class='btn btn-primary mt-2'>Bestellen!</button></div></form></div></div>"
+}
