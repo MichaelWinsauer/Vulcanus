31e23dd20ae49f50e3365dddd834c862a799d28c
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-06-03 22:32:54 +0200

    Orderlist implemented
    Former-commit-id: 9718b81368c4eb3bdad84f53ecdd28ba495760ad

--- Pizzabestelldienst/js/script.js
+++ Pizzabestelldienst/js/script.js
@@ -30,5 +30,12 @@ $(document).ready(function() {
 });
 
 function createCard(name, zutaten, preisK, preisG) {  
-    return "<div class='card my-5 mx-2' style='width: 18rem;'><img src='../images/" + name +".jpg' class='card-img-top'><div class='card-body'><h4 class='card-title pb-2'>" + name +"</h4><h6 class='card-text'>Zutaten:</h6></div><ul class='list-group list-group-flush'><li class='list-group-item'>" + zutaten + "</li></ul><div class='card-body'><form><div class='d-flex flex-column flex-wrap form-group'><div class='d-flex justify-content-around'><div class='form-group'><select class='form-control' id='Select" + name + "'><option>28cm - "+ preisK + "</option><option>32cm - "+ preisG + "</option></select></div></div><button class='btn btn-primary mt-2'>Bestellen!</button></div></form></div></div>"
+    return "<div class='card my-5 mx-2' style='width: 18rem;'><img src='../images/" + name +".jpg' class='card-img-top'><div class='card-body'><h4 class='card-title pb-2'>" + name +"</h4><h6 class='card-text'>Zutaten:</h6></div><ul class='list-group list-group-flush'><li class='list-group-item'>" + zutaten + "</li></ul><div class='card-body'><form><div class='d-flex flex-column flex-wrap form-group'><div class='d-flex justify-content-around'><div class='form-group'><select class='form-control' id='Select" + name + "'><option>28cm - "+ preisK + "</option><option>32cm - "+ preisG + "</option></select></div></div></div></form><button onclick='addToOrder(this)' class='btn btn-primary mt-2'>Bestellen!</button></div></div>"
+}
+
+function addToOrder(sender) {
+    var name = sender.parentNode.parentNode.children[1].children[0].innerHTML;
+    var selection = document.getElementById('Select' + name).options[document.getElementById('Select' + name).selectedIndex].value;
+
+    document.getElementById('OrderList').innerHTML += "<div class='form-group'><input type='text' class='form-control' value='" + name + " - " + selection + "' ></div>";
 }
